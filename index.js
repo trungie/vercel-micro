e768cafb9836c201f8438aa268fbb8f76651358b
Trung Hoang <trungiee@gmail.com>
2024-04-19 11:07:20 +1000

    add path

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+const { send } = require('micro');
+const url = require('url');
+
 module.exports = (req, res) => {
-  console.log('Request received');
-  res.end('Hello World!');
+  const parsedUrl = url.parse(req.url, true);
+  const pathname = parsedUrl.pathname;
+  
+  // Only handle requests to /jujuchella2024
+  if (pathname === '/test2024') {
+    res.writeHead(200);
+    res.end('Hello');
+  } else {
+    // Return a 404 error for any other endpoint
+    send(res, 404, { error: "Not Found" });
+  }
 };
 
